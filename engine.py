150e1a074f9df80bb6a08e480f80339c00795a38
XenonLab-Studio <xenonlab.develop@gmail.com>
2018-11-14 13:59:34 +0100

    Drawing the '@' symbol and moving it around

--- engine.py
+++ engine.py
@@ -21,10 +21,66 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import libtcodpy as libtcod
+from input_handlers import handle_keys
 
 
 def main():
-    print('Hello World!')
+    # variables for the size of the terminal
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+    # libtcod font to use. The bit 'arial10x10.png'
+    # is the file from which it reads characters and symbols.
+    libtcod.console_set_custom_font('img/arial12x12.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    # This line creates the screen.
+    # I assign the values screen_width and screen_height (80 and 50, respectively),
+    # along with a title for the window, and a Boolean value that tells libtcod whether
+    # to go in fullscreen mode or not.
+    libtcod.console_init_root(screen_width, screen_height, 'The Dungeon of Sin', False)
+    
+    con = libtcod.console_new(screen_width, screen_height)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    # game loop
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+
+        # sets the color for the '@' symbol. The "0" in this function is the console on which it draws.
+        # The first argument is '0' (again, the console on which it draws).
+        # The next two are the x and y coordinates, in this case 1 and 1.
+        # Next, I print the symbol '@' and set the background to 'none' with libtcod.BKGND_NONE.
+        libtcod.console_set_default_foreground(con, libtcod.white)
+        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+        
+        libtcod.console_flush()
+
+        #libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            player_x += dx
+            player_y += dy
+
+        if exit:
+            return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+            return True
 
 if __name__ == '__main__':
-    main()
+     main()
